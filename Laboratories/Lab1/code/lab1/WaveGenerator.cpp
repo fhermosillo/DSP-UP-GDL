c6937adcf024457ed464232e602837ad5c919398
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2023-03-15 13:03:01 -0600

    Update WaveGenerator.cpp

--- Laboratories/Lab1/code/lab1/WaveGenerator.cpp
+++ Laboratories/Lab1/code/lab1/WaveGenerator.cpp
@@ -62,13 +62,18 @@ WaveGenertor::WaveGenertor(float Fs)
   mNhalf = floor((float)mN/2.0F);
   mA = 1.0F;
   mWaveform = WAVEFORM_NONE;
+  mRT = false;
+  mFd = mF/mFs;
   
 }
 
-void WaveGenertor::setWaveform(waveform_t wave)
+void WaveGenertor::setWaveform(waveform_t wave, bool rt)
 {
   int i;
   mWaveform = wave;
+  mRT = (mN > 1024 ? true : rt);
+  if(mRT) return;
+  
   switch(wave)
   {
     case WAVEFORM_SINE:
@@ -121,6 +126,7 @@ void WaveGenertor::setFrequency(float F)
   mF = F;
   mN = ceil(mFs/mF);
   mNhalf = floor((float)mN/2.0F);
+  mFd = mF/mFs;
 }
 
 void WaveGenertor::setAmplitude(float A)
@@ -132,7 +138,48 @@ float WaveGenertor::sample(uint32_t n) {
   if(mWaveform == WAVEFORM_NOISE)
     return mA*((float)random(32768)/32768.0F - 0.5F) * 2.0;
   else
-    return mA*buf[n % mN];
+  {
+    if(mRT)
+      return sampleRT(n);
+    else
+      return mA*buf[n % mN];
+  }
+    
+}
+
+float WaveGenertor::sampleRT(uint32_t n)
+{
+  float k = mFd*n;
+  switch(mWaveform)
+  {
+    case WAVEFORM_SINE:
+      return sinf(2.0F * 3.1416F * mF/mFs * n);
+    break;
+
+    case WAVEFORM_COSINE:
+      return cosf(2.0F * 3.1416F * mF/mFs * n);
+    break;
+
+    case WAVEFORM_SAWTOOTH:
+      return 2.0F*(k - floor(0.5F + k));
+    break;
+
+    case WAVEFORM_TRIANGULAR:
+      return 4.0F*abs(k - floor(0.5F + k)) - 1;
+    break;
+
+    case WAVEFORM_SQUARE:
+      return (n < mNhalf ? 1.0F : -1.0F);
+    break;
+
+    default:
+        return 0.0F;
+    break;
+  }
+}
+
+void WaveGenertor::setRealTime(bool _rt) {
+  mRT = _rt;
 }
 
 WaveGenertor::~WaveGenertor() {
